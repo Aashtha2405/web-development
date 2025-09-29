f2ff39cb23c2d7542874dd640c0b24a351e59207
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-09-29 16:57:12 +0530

    Add files via upload

---
+++ video54/script.js
@@ -0,0 +1,16 @@
+alert("Hello world")
+
+console.log("console is running")
+
+var a = prompt("Enter your number")
+
+var isTrue = confirm("are you sure you want to leave this page and blast yourr computer")
+
+if(isTrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+
+console.log("your numbet is " + a)
---
+++ video55/script.js
@@ -0,0 +1,40 @@
+console.log("hlo this is javascript leacture")
+
+var a = 5;
+var b = 6;
+
+let c = "Aastha"
+
+ console.log(a + b + 8)
+ console.log(typeof a, typeof b, typeof c)
+
+//  const a1 = 6;
+//  a1 = a1 + 3; this is not allowed because a1 is constant
+
+// concept of var and let  in javascript prefers notes of cwh for this
+
+//data types primitive data types and objects
+//primitive data types are as follow->null,number,strung,symbol,undefined,boolean and bigInt
+
+
+let x  = "Aastha bhai";
+let y = 22;
+let z = 3.55;
+let q = undefined;
+let r = null;
+const p = true;
+
+console.log(x,y,z,q,r,p)
+console.log(typeof x, typeof y, typeof z, typeof q, typeof r, typeof p);
+
+// typeof null is considered as object in javascript
+
+
+//object
+let o = {
+    "namd":"harry",
+    "job code":453
+}
+console.log(o)
+o.salary = "100crores"
+console.log(o)
