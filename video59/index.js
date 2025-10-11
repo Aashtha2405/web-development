0214dfbadf9ab7f2e46fade1bcfaebf4ab863644
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-10-11 23:52:51 +0530

    Add files via upload

---
+++ video59/index.js
@@ -0,0 +1,12 @@
+//exercise
+//create a faulty calculator using javascript.
+
+/*This faulty calculator does following
+1.it takes two numbers as input from user
+2.it performs wrong operation
+3. + -----> -
+4. * -----> +
+5. - ----> /
+6. / -----> **
+it performs  wrong operation 10% of the times
+*/
---
+++ video60/index.js
@@ -0,0 +1,26 @@
+console.log("this is string tutorial")
+let a = "Aastha"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a.length)
+
+let real_name = "Aastha"
+let friend = "jaya"
+console.log("her name is "+real_name + " and her friend name is " + friend)
+console.log(`her name is ${real_name} and her friend name is ${friend}`)
+
+let b = "rahul"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+// console.log(b.slice(1,5))
+console.log(b.replace("rahul","romi"))
+
+
+//many more string functions are there just search on google
+//"javascript string functions"
+
