94d3a1cd7cc7a01c67f23fbfa5f16561b129722b
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-11-26 00:43:55 +0530

    Add files via upload

---
+++ video62/index.js
@@ -0,0 +1,17 @@
+//create a business name generator by combining list of adjectives and shop name and a random word
+
+//crazy 
+//amazing
+//fire
+
+
+//shop name:
+//engine
+//foods
+//garments
+
+//another word:
+//bros
+//inc
+//hub
+//limited
---
+++ video63/index.js
@@ -0,0 +1,25 @@
+//arrays
+let arr = [1,2,3,4,5]
+arr[0] = 5666;
+// console.log(arr,typeof arr);
+// console.log(arr);
+// console.log(arr.length)
+// console.log(arr[0])
+
+// console.log(arr.toString());
+// console.log(arr.join(" and "));
+// console.log(arr.pop(),arr);
+// console.log(arr.push(6),arr);
+
+
+//take help from notes of cwHarry
+//shift and unshift
+//delete
+//sort
+//splice
+//concat
+//slice
+//loops 
+///map
+//filter
+//reduce
